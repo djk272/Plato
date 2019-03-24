b25fc9f03f9992f13c5cf268e581990b48a091c1
fullsalvo <escambiadrummer@gmail.com>
2019-03-24 03:03:36 -0400

    Refactor the base code and add a bit to the readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Plato
-Random musical verse generator
 
-A Python script that scrapes a wikipdia page on Platonism. Using those words as a basis to generate phrases to be used for verses of a song.
-The phrases will be unique and generated randomly via Markov Chain.
+A random musical verse generator written in Python
+
+Scrapes [a wikipedia article on Platonism](https://en.wikipedia.org/wiki/Platonism), using the blockquotes as the source for the lyric generation. Phrases are unique and are chosen via Markov chains.
+
+## Dependencies
+
+All dependencies are available via PyPI:
+* BeautifulSoup
+* lxml
+* numpy
+* pandas
+* requests
--- scrape.py
+++ scrape.py
@@ -1,73 +1,87 @@
-### Random musical verse generator using an excerpt from Plato's middle period metaphysics from wikipedia ###
+#!/usr/bin/env python3
+"""
+Random music verse generator formed from an excerpt of Plato's metaphysics, found on Wikipedia
 
+Author: djk272
+Modified by fullsalvo
+"""
 
-### First, scrape wikipedia for the excerpt
-
-### Second, use those words to randomly generate exsistential phrases for the verse of a song
-##########################################################################################
-
-###~~Web scraper in Python 3~~###
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # Python3 Web Scraper
 import requests
 import numpy as np
-import pandas as pd 
-#################################
+import pandas as pd
 
-source = requests.get('https://en.wikipedia.org/wiki/Platonism').text #loads the website html in text form
+class Scraper:
+    """
+    Grabs and processes data obtained from the web to be used with the Markov chain generator later.
+    """
+    def __init__(self, url,fmt='lxml'):
+        source = requests.get(url).text        # downloads the webpage html as plaintext
+        self.soup = BeautifulSoup(source, fmt) # loads the lxml parser
+        # TODO: Make the scraping method and `fmt' depend on the domain name of the site in question
+        if (url == 'https://en.wikipedia.org/wiki/Platonism'):
+            self.words = self.plato()
 
-soup = BeautifulSoup(source, 'lxml') #loads the lxml parser
+    def plato(self):
+        """
+        Handles the test parser url.
+        """
+        # obtains the "blockquote" item
+        blockquote = self.soup.find('blockquote')
+        # extracts the element of interest from a list of 3 elements, all elements being strings of text. Then removes the first 11 characters from the chosen string becasue they aren't needed.
+        glaucon = blockquote.contents[1].text[11:]
+        # remove unnecessary special characters and quotation marks, because we only want words.
+        glaucon = glaucon.replace("\"","").replace("?", "").replace(",", "").replace(".","").replace("-","")
+        # split the string of text into a list of individual words
+        words = glaucon.split()
+        return words
 
-blockquote = soup.find('blockquote') #parses through finding the "blockquote" item
+class Markov:
+    """
+    Produces Markov chain lyrics from data obtained from the scraper.
+    """
+    def __init__(self,url):
+        self.scraper = Scraper(url)
+        self.pairs = self.make_pairs(self.scraper.words)
+        self.word_dict = self.form_dict()
+        self.generated = self.generate()
+        print(self.generated)
 
+    def make_pairs(self,words):
+        """
+        Form word pairs, so that predictions may later be made about what words are reasonable to follow.
+        """
+        for i in range(len(words)-1):
+            yield (words[i], words[i+1])
 
-glaucon = blockquote.contents[1].text[11:] #extracts the element of interest from a list of 3 elements, all elements being strings of text. Then removes the first 11 characters from the chosen string becasue they aren't needed.
+    def form_dict(self):
+        word_dict = {} # intialize dictionary with empty entries
 
+        for word, next_word in self.pairs:
+            if word in word_dict.keys():
+                word_dict[word].append(next_word) # create a dictionary to store a list of all the intances a word appears directly after a particular word.
+            else:
+                word_dict[word] = [next_word] #if not then they must be the same word
+        return word_dict
 
-glaucon = glaucon.replace("\"","").replace("?", "").replace(",", "").replace(".","").replace("-","") #removing unessecary special characters and quotation marks, because we only want words.
+    def generate(self):
+        # pick the first word of the generated chain, ensuring that the word chosen is also a word that starts a line in the source
+        first_word = np.random.choice(self.scraper.words) # the first word is randomly chosen from the text
+        while first_word.islower():
+            first_word = np.random.choice(self.scraper.words)
 
-words = glaucon.split() #spliting the string of text into a list of individual words
+        chain = [first_word]
 
-#print(words)
-##########################################################################################
+        n_words = 30 # the number of words the chain will be in length
 
-###~~Markov Chain Random Word Generator~~###
+        for i in range(n_words):
+            chain.append(np.random.choice(self.word_dict[chain[-1]])) # add 30 random words from the dictonary, the next word is chosen based on the last element or word in the chain.
 
-## Building a function to make the words in the string into pairs of words, so we can predict which word should come after any given word ##
+        return ' '.join(chain) # joins the words in the list made in the generated chain of words in to a string, with all the words connected by a space
 
-def make_pairs(words):
+def main():
+    plato = Markov('https://en.wikipedia.org/wiki/Platonism')
+    quit()
 
-	for i in range(len(words)-1):
-		yield (words[i], words[i+1]) # all of this in the function creates a list which pairs adjecent words together, excluding the first word of the previous pair, so you create unique pairs
-
-
-pairs = make_pairs(words) # word pairs are now equal to the previous function
-
-## building the dictionary ##
-
-word_dict = {} # intialize dictionary with empty entries
-
-
-for word, next_word in pairs:
-	if word in word_dict.keys():
-		word_dict[word].append(next_word) # create a dictionary to store a list of all the intances a word appears directly after a particular word.
-	else:
-		word_dict[word] = [next_word] #if not then they must be the same word
-
-#print(word_dict["the"])# prints all of the words that come after the word "the" in the text
-
-
-first_word = np.random.choice(words) # the first word is randomly chosen from the text
-
-while first_word.islower():
-	first_word = np.random.choice(words) # all this does is when the first word chosen from the text is lower case it will chose the first word randomly again until it isn't. This prevents a word being chosen mid sentence in the text that is probably awkward to use at the begining of a new sentence.
-
-chain = [first_word] # we start the chain of words we want to generate with the first word randomly chosen
-
-n_words = 30 # the number of words the chain will be in length
-
-for i in range(n_words):
-	chain.append(np.random.choice(word_dict[chain[-1]])) # add 30 random words from the dictonary, the next word is chosen based on the last element or word in the chain.
-
-' '.join(chain) # joins the words in the list made in the generated chain of words in to a string, with all the words connected by a space
-
-print(' '.join(chain)) # prints the output so you can read it
+if __name__ == '__main__':
+    main()
