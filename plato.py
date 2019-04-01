47ecaafdb25db330f3595dd6109e0e450768e9bf
fullsalvo <escambiadrummer@gmail.com>
2019-04-01 12:08:11 -0400

    introduce album grabbing as an option to genius grabbing (sometimes one song doesn't provide enough source material for meaningful Markov chains), clean up removal of special characters

--- plato.py
+++ plato.py
@@ -13,21 +13,25 @@ import pandas as pd
 import re
 import requests
 
-# create a global variable to be used later as the argument handling object
-_args = None
+_lyric_section_headers = r'\[[^\]]*\]\n'
+_remove_lyric_headers = lambda x: re.sub(_lyric_section_headers, '', x)
+_remove_special_characters = lambda x: re.sub(r'[\\,\.\?()"]', '', x)
 
 class Scraper:
     """
     Grabs and processes data obtained from the web to be used with the Markov chain generator later.
     """
-    def __init__(self, url, fmt='lxml'):
+    def __init__(self, url, album=False, fmt='lxml'):
         source = requests.get(url).text        # downloads the webpage html as plaintext
         self.soup = BeautifulSoup(source, fmt) # loads the lxml parser
         # TODO: Make the scraping method and `fmt' depend on the domain name of the site in question
         if (url == 'https://en.wikipedia.org/wiki/Platonism'):
             self.words = self.plato()
         if (url.startswith('https://genius.com/')):
-            self.words = self.genius()
+            if album:
+                self.album()
+            else:
+                self.words = self.genius()
 
     def plato(self):
         """
@@ -38,27 +42,41 @@ class Scraper:
         # extracts the element of interest from a list of 3 elements, all elements being strings of text. Then removes the first 11 characters from the chosen string becasue they aren't needed.
         glaucon = blockquote.contents[1].text[11:]
         # remove unnecessary special characters and quotation marks, because we only want words.
-        glaucon = glaucon.replace("\"","").replace("?", "").replace(",", "").replace(".","").replace("-","")
+        glaucon = _remove_special_characters(glaucon)
         # split the string of text into a list of individual words
         words = glaucon.split()
         return words
 
-    def genius(self):
-        lyrics = self.soup.p.get_text()
-        section_headers = r'\[[^\]]*\]\n'
-        lyrics = re.sub(section_headers, '', lyrics)
-        words = lyrics.split()
-        return words
+    def genius(self,url=None):
+        if url:
+            source = requests.get(url).text
+            soup = BeautifulSoup(source, 'lxml')
+            lyrics = _remove_lyric_headers(soup.p.get_text())
+            lyrics = _remove_special_characters(lyrics)
+            words = lyrics.split()
+            return words
+        else:
+            lyrics = _remove_lyric_headers(self.soup.p.get_text())
+            lyrics = _remove_special_characters(lyrics)
+            words = lyrics.split()
+            return words
+
+    def album(self):
+        tracklist = self.soup.find('div', {'class': 'track_listing'})
+        lyrics = self.genius()
+        setattr(self,'words',lyrics)
+        for a in tracklist.find_all('a'):
+            self.words.extend(self.genius(a['href']))
 
 class Markov:
     """
     Produces Markov chain lyrics from data obtained from the scraper.
     """
-    def __init__(self, url, n_words=30, to_print=True):
-        self.scraper = Scraper(url)
+    def __init__(self, url, n_words=30, album=False, to_print=True):
+        self.scraper = Scraper(url,album)
         self.n_words = n_words
-        self.pairs = self.make_pairs(self.scraper.words)
-        self.word_dict = self.form_dict()
+        pairs = self.make_pairs(self.scraper.words)
+        self.word_dict = self.form_dict(pairs)
         self.generated = self.generate()
         if to_print:
             print(self.generated)
@@ -70,10 +88,10 @@ class Markov:
         for i in range(len(words)-1):
             yield (words[i], words[i+1])
 
-    def form_dict(self):
+    def form_dict(self, pairs):
         word_dict = {} # intialize dictionary with empty entries
 
-        for word, next_word in self.pairs:
+        for word, next_word in pairs:
             if word in word_dict.keys():
                 word_dict[word].append(next_word) # create a dictionary to store a list of all the intances a word appears directly after a particular word.
             else:
@@ -112,11 +130,18 @@ def _parse_args():
                         """,
                         type=str,
                         default="https://en.wikipedia.org/wiki/Platonism")
+    parser.add_argument('-a','--album',
+                        help="""
+                        Rather than only grabbing a song's lyrics, grab the entire album.
+                        """,
+                        dest='album',
+                        action='store_true',
+                        default=False)
     return parser.parse_args()
 
 def main():
     _args = _parse_args()
-    plato = Markov(_args.url,n_words=_args.words)
+    plato = Markov(_args.url,n_words=_args.words,album=_args.album)
     # TODO: Interactively allow further generated chains to be created before completely exiting
     # (so data doesn't have to be re-downloaded multiple times for the same set)
 
